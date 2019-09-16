251275e3a768cbe5bf79d1a66ef15c532fcfb649
Shinichi Ozaki <sdkfz181tiger@gmail.com>
2019-09-16 14:51:06 +0900

    神社に

--- docs/app/index.js
+++ docs/app/index.js
@@ -144,8 +144,8 @@ IndexCtrl = {
             popupAnchor: [0, -34],
         }),
         shrine: L.icon({
-            iconUrl: './img/park.png',
-            iconRetinaUrl: './img/park.png',
+            iconUrl: './img/shrine.png',
+            iconRetinaUrl: './img/shrine.png',
             iconSize: [32, 32],
             iconAnchor: [16, 32],
             popupAnchor: [0, -32],
