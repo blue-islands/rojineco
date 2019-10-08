3d232de21b7e8c0fc597b9d96716a08c47691fbf
blue_islands <h.aoshima@livlog.jp>
2019-10-09 01:56:39 +0900

    no message

--- docs/app/util.js
+++ docs/app/util.js
@@ -159,21 +159,21 @@ var Util = {
                         break;
                 }
                 
-                // if(drawWidth > drawHeight){
-                //     // 横長の画像は横のサイズを指定値にあわせる
-                //     var ratio = drawHeight / drawWidth;
-                //     drawWidth = Util.THUMBNAIL_WIDTH;
-                //     drawHeight = Util.THUMBNAIL_WIDTH * ratio;
-                // } else {
-                //     // 縦長の画像は縦のサイズを指定値にあわせる
-                //     var ratio = drawWidth / drawHeight;
-                //     drawWidth = Util.THUMBNAIL_HEIGHT * ratio;
-                //     drawHeight = Util.THUMBNAIL_HEIGHT;
-                // }
+                if(drawWidth > drawHeight){
+                    // 横長の画像は横のサイズを指定値にあわせる
+                    var ratio = drawHeight / drawWidth;
+                    drawWidth = Util.THUMBNAIL_WIDTH;
+                    drawHeight = Util.THUMBNAIL_WIDTH * ratio;
+                } else {
+                    // 縦長の画像は縦のサイズを指定値にあわせる
+                    var ratio = drawWidth / drawHeight;
+                    drawWidth = Util.THUMBNAIL_HEIGHT * ratio;
+                    drawHeight = Util.THUMBNAIL_HEIGHT;
+                }
 
                 ctx.drawImage(_img, 0, 0, drawWidth, drawHeight);
-                canvas.width = drawWidth;
-                canvas.height = drawHeight;
+                // canvas.width = drawWidth;
+                // canvas.height = drawHeight;
                 // Destination Image
                 var imgB64Dst = canvas.toDataURL(_imgType);
                 callback(imgB64Dst);
