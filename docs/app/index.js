ce18c9d4bf6bb618807ec058db97ffd594b64975
blue_islands <h.aoshima@livlog.jp>
2019-10-19 17:14:16 +0900

    no message

--- docs/app/index.js
+++ docs/app/index.js
@@ -1016,6 +1016,7 @@ IndexCtrl = {
                         }
                     }).fail(function(jqXHR, textStatus, errorThrown) {
                         logger.error(errorThrown);
+                        alert('エラーが発生しました。');
                     }).always(function() {
                         //     logger.info('***** 処理終了 *****');
                         $('#fileUpload').val('');
