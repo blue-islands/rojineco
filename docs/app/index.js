99a73de68763feaecaf079ebc01897af89f2a187
blue_islands <h.aoshima@livlog.jp>
2020-01-18 21:36:47 +0900

    no message

--- docs/app/index.js
+++ docs/app/index.js
@@ -18,7 +18,7 @@ IndexCtrl = {
     RANGE_DISTANCE: 10000,
     GET_DISTANCE: 1000,
     BOUND_ZOOM: 13,
-    CATDETECTOR_ITERATION: 'Iteration25',
+    CATDETECTOR_ITERATION: 'Iteration27',
     WHATCAT_ITERATION: 'Iteration11',
     userId: null,
     photoId: null,
